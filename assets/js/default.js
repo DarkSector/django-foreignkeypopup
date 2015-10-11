549ee72083d2d88c81231d1ccce25a2a42ea2b69
Pronoy Chopra <pronoyc@gmail.com>
2015-10-11 13:45:35 -0500

    Updated js with correct class and id names; Added new method to detect trigger

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var current_id;
     var a_id;
-    $.each($(".fk-popup"),function(index, val){
-        current_id = val.attr('id');
-        a_id = current_id + 'trigger' + index;
-        $('<a href="#" class="fk-popup-btn" id="'+a_id+'">+</a>').insertAfter($(this));
+    $.each($(".fk-popup"), function (index) {
+        current_id = $(this).attr('id');
+        a_id = current_id + '_fktrigger_' + index;
+        $('<a href="#" class="fk-popup-btn" id="' + a_id + '">+</a>').insertAfter($(this));
+    });
+    $(".fk-popup-btn").on("click", function(){
+        // Any time the button gets triggered
+        // open a new window and go to the link
     });
 });
