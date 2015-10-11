28bdae0e74f3408d9e628188aa73418f8666abd1
Pronoy Chopra <pronoyc@gmail.com>
2015-10-11 13:36:11 -0500

    Added methods to add fk popups

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+    var current_id;
+    var a_id;
+    $.each($(".fk-popup"),function(index, val){
+        current_id = val.attr('id');
+        a_id = current_id + 'trigger' + index;
+        $('<a href="#" class="fk-popup-btn" id="'+a_id+'">+</a>').insertAfter($(this));
+    });
 });
