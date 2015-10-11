bdf76efdfea04b1118fbc0532ca5528215182965
Pronoy Chopra <pronoyc@gmail.com>
2015-10-11 03:22:43 -0500

    Removed djmoney

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'testapp',
     # 'debug_toolbar',
-    'djmoney'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,7 +46,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddlewre',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
