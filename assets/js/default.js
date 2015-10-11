19ea97fed221dedffa168979269995d9fe73316f
Pronoy Chopra <pronoyc@gmail.com>
2015-10-11 13:55:45 -0500

    Added data-url to the <a>

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     var current_id;
     var a_id;
+    var data_url;
     $.each($(".fk-popup"), function (index) {
         current_id = $(this).attr('id');
         a_id = current_id + '_fktrigger_' + index;
-        $('<a href="#" class="fk-popup-btn" id="' + a_id + '">+</a>').insertAfter($(this));
+        data_url = $(this).attr('data-url');
+        $('<a href="#" data-url="'+data_url+'" class="fk-popup-btn" id="' + a_id + '">+</a>').insertAfter($(this));
     });
     $(".fk-popup-btn").on("click", function(){
         // Any time the button gets triggered
